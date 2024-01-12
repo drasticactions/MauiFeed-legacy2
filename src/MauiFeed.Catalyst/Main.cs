294ad60555fe051d1ac27b65415fd045ed125e2e
Tim Miller <innerlogic4321@gmail.com>
2024-01-12 16:34:18 +0900

    reset

--- src/MauiFeed.Catalyst/Main.cs
+++
@@ -1,9 +0,0 @@
-// <copyright file="Main.cs" company="Drastic Actions">
-// Copyright (c) Drastic Actions. All rights reserved.
-// </copyright>
-
-using MauiFeed.Catalyst;
-using UIKit;
-
-DrasticForbiddenControls.CatalystControls.AllowsUnsupportedMacIdiomBehavior();
-UIApplication.Main(args, null, typeof(AppDelegate));
